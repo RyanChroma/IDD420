c2f33fe2b4c5679992944e8243eb3c86f866cad6
RyanCua <ttff2ryan@gmail.com>
2022-08-11 12:08:06 +0700

    First Commit

---
+++ Hot and Thick/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+	public void ChangeScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
