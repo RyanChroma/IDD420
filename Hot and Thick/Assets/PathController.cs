fd6d8b5730a58dc6886e9a1274fecf097761c3e1
CapBear <54356212+sand32848@users.noreply.github.com>
2022-08-16 22:14:39 +0700

    Update ghost walking

--- Hot and Thick/Assets/PathController.cs
+++ Hot and Thick/Assets/PathController.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PathController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<Transform> wayPoint = new List<Transform>();
+    private NavMeshAgent navMeshAgent;
+    private int wayPointIndex;
+
+    private void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        navMeshAgent.destination = wayPoint[0].position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(navMeshAgent.remainingDistance <= 0.5f)
+        {
+            wayPointIndex++;
+
+            if(wayPointIndex > wayPoint.Count - 1)
+            {
+                navMeshAgent.destination = wayPoint[0].position;
+                wayPointIndex = 0;
+            }
+            else
+            {
+                navMeshAgent.destination = wayPoint[wayPointIndex].position;
+            }
+            
+        }
     }
+
 }
---
+++ Hot and Thick/Assets/Scripts/GhostVisibility.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostVisibility : MonoBehaviour
+{
+    [SerializeField]private MeshRenderer renderer;
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        GhostCamera.onCamOn += Visible;
+        GhostCamera.onCamOff += Invisible;
+    }
+
+    private void OnDisable()
+    {
+        GhostCamera.onCamOn -= Visible;
+        GhostCamera.onCamOff -= Invisible;
+    }
+
+    public void Invisible()
+    {
+        renderer.material.color = new Color32(0,0,0,0);
+    }
+
+    public void Visible()
+    {
+        renderer.material.color = new Color32(255, 255, 255, 255);
+    }
+}
