d77b9260253376228ef3838f23733d7cd5f1168c
RyanCua <ttff2ryan@gmail.com>
2022-09-05 11:18:12 +0700

    Lighting and enemy edit

--- Assets/Scripts/GhostVisibility.cs
+++ Assets/Scripts/GhostVisibility.cs
@@ -8,6 +8,7 @@ public class GhostVisibility : MonoBehaviour
     void Start()
     {
         renderer = GetComponent<MeshRenderer>();
+        //renderer.enabled = false;
         CameraToggle.onCamOn += Visible;
         CameraToggle.onCamOff += Invisible;
     }
@@ -20,11 +21,13 @@ public class GhostVisibility : MonoBehaviour
 
     public void Invisible()
     {
+        //renderer.enabled = false;
         renderer.material.color = new Color32(0, 0, 0, 0);
     }
 
     public void Visible()
     {
+       // renderer.enabled = true;
         renderer.material.color = new Color32(255, 255, 255, 255);
     }
 }
